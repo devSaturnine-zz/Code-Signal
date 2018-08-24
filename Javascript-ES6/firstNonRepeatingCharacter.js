a3dd6c668fa5763b9017efaddf05269724cb5bf9
devSaturnine <neil.dolley@gmail.com>
2018-08-25 00:04:12 +0200

    Update firstNonRepeatingCharacter.js

--- Javascript-ES6/firstNonRepeatingCharacter.js
+++ Javascript-ES6/firstNonRepeatingCharacter.js
@@ -26,7 +26,8 @@ function firstNotRepeatingCharacter(s) {
 }
 
 
-/* Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+/* Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be 
+asked to do during a real interview.
 
 Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
 
