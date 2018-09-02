adf5c5685b0e85d35eacdfea6f40a58a38ab038c
devSaturnine <neil.dolley@gmail.com>
2018-09-02 15:11:48 +0200

    Update isListPalindrome.js

--- Javascript-ES6/isListPalindrome.js
+++ Javascript-ES6/isListPalindrome.js
@@ -65,9 +65,8 @@ function isListPalindrome(l) {
 }
 
 
-
-Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in l, since this is what 
-you'll be asked to do during an interview.
+/*
+Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in l.
 
 Given a singly linked list of integers, determine whether or not it's a palindrome.
 
@@ -92,3 +91,4 @@ Guaranteed constraints:
 [output] boolean
 
 Return true if l is a palindrome, otherwise return false.
+*/
